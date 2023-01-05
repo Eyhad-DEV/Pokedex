d8e385ae7dea8cd406d06a9ae96eb09b9fa7833e
Matheus Eyhad <120788602+Eyhad-DEV@users.noreply.github.com>
2023-01-05 09:59:32 -0300

    Atualizando dex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,22 @@
+function convertPokemonTypeToLi{
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 
 function convertPokemonToLi(pokemon){
     return `
         <li class="pokemon">
-
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
-
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
-
-
+                 ${convertPokemonTypesToLi(pokemon.types).join('')}
                  </ol>
-
         
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"alt= "${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    alt= "${pokemon.name}">
             </div>
     
-
         </li>`
 
 }
@@ -35,5 +32,4 @@ const pokemonOl = document.getElementById('pokemonList')
         }
     }) 
     .catch((erro) => console.error(error))
-  
   
